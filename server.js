2b7edacb27b9f46e4edb90508244d2eaa92d8cb9
김주엽 <kjyy08@naver.com>
2025-02-10 12:53:00 +0900

    express & cors

---
+++ server.js
@@ -0,0 +1,15 @@
+// https://expressjs.com/
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
