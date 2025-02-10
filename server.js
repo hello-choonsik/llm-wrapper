dfb7a902433b1ca004bfbb897488162ff151a517
김주엽 <kjyy08@naver.com>
2025-02-10 14:28:50 +0900

    ✨ Feat: form 추가

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/", (req, res) => {
+  const { body } = req;
+  const { text } = body;
+
+  res.json({ text });
 });
 
 app.listen(port, () => {
